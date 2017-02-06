d987a94f719f1ff2f84db04d5026f966c4c6441c
John David Osborne <ozborn@uab.edu>
2017-02-06 17:03:48 -0600

    Added ability to filter development SemEval 2015 files for CUI-less
    Some cleanup, more work could be done but not worth it.

--- src/main/java/edu/uab/ccts/nlp/shared_task/semeval2015/uima/annotator/SemEval2015ViewCreatorAnnotator.java
+++ src/main/java/edu/uab/ccts/nlp/shared_task/semeval2015/uima/annotator/SemEval2015ViewCreatorAnnotator.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -35,13 +33,13 @@ import edu.uab.ccts.nlp.shared_task.semeval2015.SemEval2015Constants;
 public class SemEval2015ViewCreatorAnnotator extends JCasAnnotator_ImplBase {
 
 
-	public static final String PARAM_TRAINING_PATH = "SemEval2015TrainingPath";
+	public static final String PARAM_TRAINING_PATH = "FilesForViewPath";
 	@ConfigurationParameter(
 			name = PARAM_TRAINING_PATH,
-			description="Path of SemEval 2015 Training files",
+			description="Path of SemEval 2015 files for which view will be created for ",
 			mandatory=true
 	)
-	private String SemEval2015TrainingPath; 
+	private String filesForViewPath; 
 	
 	public static final String PARAM_CUILESS_ONLY = "cuilessOnly";
 	@ConfigurationParameter(
@@ -59,6 +57,7 @@ public class SemEval2015ViewCreatorAnnotator extends JCasAnnotator_ImplBase {
 	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
 		LOG = this.getContext().getLogger();
+
 		JCas pipedView = null, semevalTextView = null;
 		pipedView = JCasUtil.getView(jcas, SemEval2015Constants.PIPED_VIEW, true);
 		semevalTextView = JCasUtil.getView(jcas, SemEval2015Constants.GOLD_VIEW, true);
@@ -76,58 +75,50 @@ public class SemEval2015ViewCreatorAnnotator extends JCasAnnotator_ImplBase {
 			if(type.toLowerCase().startsWith("echo")) type="echo";
 			if(type.toLowerCase().startsWith("radio")) type="radiology";
 		} 
-		LOG.log(Level.FINEST,"Type:"+type+" Prefix:"+prefix);
-		LOG.log(Level.FINE,"SemEval2015 Training Path:"+SemEval2015TrainingPath);
-		if(SemEval2015TrainingPath==null) {
-			LOG.log(Level.SEVERE,"Null value for SemEval2015TrainingPath");
+		LOG.log(Level.FINER,"Type:"+type+" Prefix:"+prefix);
+		
+		
+		LOG.log(Level.FINER,"SemEval2015 File Path:"+filesForViewPath);
+		if(filesForViewPath==null) {
+			LOG.log(Level.SEVERE,"Null value for filesForViewPath");
 			throw new AnalysisEngineProcessException();
 		}
-		String original_textfile_name = SemEval2015TrainingPath+File.separator+type+
+		
+		String original_textfile_name = filesForViewPath+File.separator+type+
 				File.separator+prefix+"."+SemEval2015Constants.SEMEVAL_TEXT_FILE_EXTENSION;
-		String pipefilename = SemEval2015TrainingPath+File.separator+type+
+		String pipefilename = filesForViewPath+File.separator+type+
 				File.separator+prefix+"."+SemEval2015Constants.SEMEVAL_PIPED_EXTENSION;
 		try {
+			
+			//Read in Text Files, Assume Training Files First
 			File ofile = new File(original_textfile_name);
 			if(ofile.exists()) {
 				String otext = FileUtils.readFileToString(ofile);
 				semevalTextView.setDocumentText(otext);
 			} else {
-				LOG.log(Level.INFO,"Did not find training text file:"+ofile+", trying devel file formatting");
-				ofile = new File(SemEval2015Constants.defaultDevelPath+File.separator+type+
-						File.separator+prefix.split("\\.")[0]+"."+SemEval2015Constants.SEMEVAL_TEXT_FILE_EXTENSION);
-				if(ofile.exists()){
-					String otext = FileUtils.readFileToString(ofile);
+				File dfile = new File(filesForViewPath+ File.separator+prefix.split("\\.")[0]+"."+
+				SemEval2015Constants.SEMEVAL_TEXT_FILE_EXTENSION);
+				LOG.log(Level.FINER,"Did not find training text file:"+ofile+", trying devel file "+dfile.toString());
+				if(dfile.exists()){
+					String otext = FileUtils.readFileToString(dfile);
 					semevalTextView.setDocumentText(otext);
 				} else {
-					LOG.log(Level.WARNING,"Could not find expected devel text file:"+ofile.getPath());
+					LOG.log(Level.WARNING,"Could not find expected devel text file:"+dfile.getPath());
 				}
 			}
+			
+			
+			//Read in Piped Files, Assume Training Files First
 			File pfile = new File(pipefilename);
 			if(pfile.exists()) {
-				//String ptext = FileUtils.readFileToString(pfile);
-					
-				try (Stream<String> stream = Files.lines(Paths.get(pipefilename),Charset.forName("UTF-8"))) {
-					String ptext = null;
-					if(cuilessOnly) { 
-							LOG.log(Level.FINE,"Doing CUILESS only");
-							ptext = stream
-							.filter(line -> line.indexOf("|CUI-less")!=-1)
-							.collect(Collectors.joining("\n"));
-					} else {
-						ptext = stream.collect(Collectors.joining("\n"));
-					}
-					pipedView.setDocumentText(ptext);
-					LOG.log(Level.FINE,"INFO document text to:"+ptext);
-				} catch (IOException ioe) { ioe.printStackTrace(); throw new AnalysisEngineProcessException(ioe);}
-
-
+				pipedView.setDocumentText(readSemEvalFile(pipefilename,cuilessOnly));
 			} else {
-				//System.out.println("Could not find expected pipe file:"+pfile.getPath());
-				pfile = new File(SemEval2015Constants.defaultDevelPath+File.separator+type+
-						File.separator+prefix.split("\\.")[0]+"."+SemEval2015Constants.SEMEVAL_PIPED_EXTENSION);
+				pipefilename= SemEval2015Constants.defaultDevelPath+File.separator+type+
+				File.separator+prefix.split("\\.")[0]+"."+SemEval2015Constants.SEMEVAL_PIPED_EXTENSION;
+				LOG.log(Level.FINE,"Did not find training pipe file:"+pfile+", trying devel pipe file "+pipefilename);
+				pfile = new File(pipefilename);
 				if(pfile.exists()) {
-					String ptext = FileUtils.readFileToString(pfile);
-					pipedView.setDocumentText(ptext);
+					pipedView.setDocumentText(readSemEvalFile(pipefilename,cuilessOnly));
 				} else {
 					LOG.log(Level.WARNING,"Could not find expected pipe file:"+pfile.getPath());
 				}
@@ -138,12 +129,33 @@ public class SemEval2015ViewCreatorAnnotator extends JCasAnnotator_ImplBase {
 	}
 
 
-	public static AnalysisEngineDescription createAnnotatorDescription(String training_path)
+	/**
+	 * @param pipedFilename
+	 * @throws AnalysisEngineProcessException
+	 */
+	private String readSemEvalFile(String pipedFilename, boolean cuilessonly) throws AnalysisEngineProcessException {
+		String ptext = null;
+		try (Stream<String> stream = Files.lines(Paths.get(pipedFilename),Charset.forName("UTF-8"))) {
+			if(cuilessonly) { 
+					LOG.log(Level.FINE,"Doing CUILESS only");
+					ptext = stream
+					.filter(line -> line.indexOf("|CUI-less")!=-1)
+					.collect(Collectors.joining("\n"));
+			} else {
+				ptext = stream.collect(Collectors.joining("\n"));
+			}
+			LOG.log(Level.FINE,"SemEval2015 file text is:"+ptext);
+		} catch (IOException ioe) { ioe.printStackTrace(); throw new AnalysisEngineProcessException(ioe);}
+		return ptext;
+	}
+
+
+	public static AnalysisEngineDescription createAnnotatorDescription(String thepath)
 		throws ResourceInitializationException {
 			return AnalysisEngineFactory.createEngineDescription(
 				SemEval2015ViewCreatorAnnotator.class,
 				SemEval2015ViewCreatorAnnotator.PARAM_TRAINING_PATH,
-				training_path
+				thepath
 			);
 	}
 
@@ -151,18 +163,18 @@ public class SemEval2015ViewCreatorAnnotator extends JCasAnnotator_ImplBase {
 	
 	/**
 	 * 
-	 * @param training_path Path of SemEval 2015 data
+	 * @param thepath Path of SemEval 2015 data files
 	 * @param createOnlyCuiless true if only CUI-less annotations should be read in
 	 * @return
 	 * @throws ResourceInitializationException
 	 */
-	public static AnalysisEngineDescription createDescription(String training_path,
+	public static AnalysisEngineDescription createDescription(String thepath,
 			boolean createOnlyCuiless)
 		throws ResourceInitializationException {
 			return AnalysisEngineFactory.createEngineDescription(
 				SemEval2015ViewCreatorAnnotator.class,
 				SemEval2015ViewCreatorAnnotator.PARAM_TRAINING_PATH,
-				training_path,
+				thepath,
 				SemEval2015ViewCreatorAnnotator.PARAM_CUILESS_ONLY,
 				createOnlyCuiless
 			);
